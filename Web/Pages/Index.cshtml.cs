e9f9182118b987cea3dfab4147f83f5350051a6b
SayeedAlMasum <githubms789@gmail.com>
2025-01-10 12:46:53 +0600

    Add project files.

---
+++ Web/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+
+namespace Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public List<string> roles { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            roles = new List<string>()
+            { "Admin", "Instructor", "Student" };
+        }
+    }
+}
